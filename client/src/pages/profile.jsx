6bb6ea711c680cc8e5a242f8e7a52f251f3dcebb
Dikshya Gouli <dikshyagouli1@gmail>
2026-05-08 14:28:31 +0545

    update

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext'; // Adjust path as needed
-import './Profile.css'; // Import the CSS file for styling
+import "./Profile.css";// Import the CSS file for styling
 
 const Profile = () => {
     const { user, loading } = useAuth();
